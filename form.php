0482229cc06e57278a4daef2c0b0fee0dbc128bf
marcll95.lg <marcll95.lg@gmail.com>
2010-03-20 08:43:32 +0700

    assembla/libgen/rev.21/Irbis
    Signed-off-by: marcll95.lg <marcll95.lg@gmail.com>

--- form.php
+++ form.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 	include 'connect.php';
 	include 'html.php';
 	// form 1 (new record) and 2 (editing) defaults
@@ -10,8 +10,10 @@
 	$publisher = '';
         $city = '';
 	$edition = '';
+        $series = '';
+        $periodical = '';
 	$pages = '';
-	$identifier = ''; //'ISBN10:'
+	$identifier = '';
 	$language = '';
 	$library = '';
 	$issue = '';
@@ -20,12 +22,10 @@
 	$color = '';
 	$cleaned = '';
 	$commentary = '';
-	$series = '';
-        $periodical = '';
+	$coverurl = '';
 	$udc = '';
 	$lbc = '';
 	$bookcode = '';
-	$coverurl = '';
 	// form 1 or 2 submitted?
 	if ($_POST['Form'] == 1)
 	{
@@ -82,6 +82,7 @@
 		$volinfo = htmlspecialchars($rows['VolumeInfo'],ENT_QUOTES);
 		$year = $rows['Year'];
 		$publisher = htmlspecialchars($rows['Publisher'],ENT_QUOTES);
+                $city = htmlspecialchars($rows['City'],ENT_QUOTES);
 		$edition = htmlspecialchars($rows['Edition'],ENT_QUOTES);
 		$pages = htmlspecialchars($rows['Pages'],ENT_QUOTES);
 		$identifier = htmlspecialchars($rows['Identifier'],ENT_QUOTES);
@@ -94,10 +95,11 @@
 		$cleaned = htmlspecialchars($rows['Cleaned'],ENT_QUOTES);
 		$commentary = htmlspecialchars($rows['Commentary'],ENT_QUOTES);
 		$series = htmlspecialchars($rows['Series'],ENT_QUOTES);
+                $periodical = htmlspecialchars($rows['Periodical'],ENT_QUOTES);
+                $coverurl = htmlspecialchars($rows['Coverurl'],ENT_QUOTES);
 		$udc = htmlspecialchars($rows['UDC'],ENT_QUOTES);
 		$lbc = htmlspecialchars($rows['LBC'],ENT_QUOTES);
 		$bookcode = htmlspecialchars($rows['BooksellingCode'],ENT_QUOTES);
-		$coverurl = htmlspecialchars($rows['Coverurl'],ENT_QUOTES);
 	} else {
 		$editing = false;
 		$mode = "<font color=green><h1>Registering a new book \\ Регистрация новой книги</h1></font>";
@@ -129,11 +131,9 @@
             $publisher = htmlspecialchars($amazonInfo['Publisher'],ENT_QUOTES);
             $edition = htmlspecialchars($amazonInfo['Edition'],ENT_QUOTES);
             $pages = htmlspecialchars($amazonInfo['Pages'],ENT_QUOTES);
-            $identifier = 'ISBN10:'.htmlspecialchars($amazonInfo['ISBN'],ENT_QUOTES);
+            $identifier = 'ISBN '.htmlspecialchars($amazonInfo['ISBN'],ENT_QUOTES);
             $language = htmlspecialchars($amazonInfo['Language'],ENT_QUOTES);
-            $commentary = htmlspecialchars($amazonInfo['Content'],ENT_QUOTES);
-            $coverurl = htmlspecialchars($amazonInfo['Image'],ENT_QUOTES);
-             
+            $commentary = htmlspecialchars($amazonInfo['Content'],ENT_QUOTES); 
                
         }   
     }
@@ -165,46 +165,17 @@
             $publisher = htmlspecialchars($ozonInfo['Publisher'],ENT_QUOTES);
             $year = htmlspecialchars($ozonInfo['Year'],ENT_QUOTES);
             $pages = htmlspecialchars($ozonInfo['Pages'],ENT_QUOTES);
-            $identifier = 'ISBN10:'.$isbn;
+            $identifier = 'ISBN '.$isbn;
             $commentary = htmlspecialchars($ozonInfo['Content'],ENT_QUOTES);
-            $topic = htmlspecialchars($ozonInfo['Topic'],ENT_QUOTES);
-            $coverurl = htmlspecialchars($ozonInfo['Image'], ENT_QUOTES);
-            $coverurl = str_replace("/small", "", $image);
-            $coverurl = str_replace(".gif", ".jpg", $image); 
+            $topic = htmlspecialchars($ozonInfo['Topic'],ENT_QUOTES); 
                
         }   
     }
     
-    //RGB
-    if(isset($_POST['rgb'])){
-        
-        
-        $filesize = $_GET['filesize'];
-        $fileext = $_GET['fileext'];
-        
-        $number = htmlspecialchars($_POST['isbn']);
-        
-        if( !(substr_count(trim($number),'-') == 3)&&(strlen(trim($number))==13) || !(substr_count(trim($number),'-') == 4)&&(strlen(trim($number))==17)){
-            
-            require_once 'ISBN-0.1.6/ISBN.php';
-            $isbn = new ISBN($number);
-            $number = substr($isbn->getISBNDisplayable(),9);
-            
-        }
-       
-        $isbn = $number;
-        
-        include 'rgbRequest.php';
-        
-    }
-    
     $isbnForm = "<form action='registration.php?md5=".$md5."&filesize=".$filesize."&fileext=".$fileext."' method='post' >
 ISBN: <input type='text' name='isbn' size='20' maxlength='25' value='".htmlspecialchars($_POST['isbn'],ENT_QUOTES)."' />
-search in: 
-<input type='submit' value='Amazon' name='amazon'/>
-<input type='submit' value='Ozon' name='ozon'/>
-<input type='submit' value='RSL' name='rgb'/>
-\t".$amazonError.$ozonError.$rgbError."</form>";
+<input type='submit' value='Info from Amazon' name='amazon'/>
+<input type='submit' value='Info from Ozon' name='ozon'/>\t".$amazonError.$ozonError."</form>";
    
     $regform = $htmlheadfocus.$isbnForm."<form action='register.php' method='post'>
 <table width=100% border=0 cellspacing=0>
@@ -221,7 +192,7 @@ search in:
 <tr><td><font face=arial size=3><b>City \\ Город</b></font><td><input type='text' name='City' size=90 value='".$city."' maxlength=200/>
 <tr><td><font face=arial size=3><b>Number of Pages \\ Число страниц</b></font><td><input type='text' name='Pages' size=5 value='".$pages."' maxlength=10/>
 <tr><td><font face=arial size=3><b><a href='http://free-books.dontexist.com/librarian/lang.txt'>Language</b> <font size=2 color=gray>(Russian, English, ...)</font></font><td><input type='text' name='Language' size=90 value='".$language."' maxlength=50/>
-<tr><td><font face=arial size=3><b>ISBN</b> <font size=2 color=gray>(только 10 или 13 цифр с '-' и ' ')</font></font><td><input type='text' name='Identifier' size=20 value='".$identifier."' maxlength=100/>
+<tr><td><font face=arial size=3><b>ISBN</b> <font size=2 color=gray>(напр.:ISBN10:0-0000-0000-0;ISBN13:978-0-0000 0000-0)</font></font><td><input type='text' name='Identifier' size=20 value='".$identifier."' maxlength=100/>
 <tr><td><font face=arial size=3><b>Library \\ Библиотека</b> <font size=2 color=gray>(kolxoz, homelab, ...)</font></font><td><input type='text' name='Library' size=10 value='".$library."' maxlength=50/>
 <tr><td><font face=arial size=3><b>Issue \\ Издание библиотеки</b> <font size=2 color=gray>(DVD-, release №, ...)</font></font><td><input type='text' name='Issue' size=5 value='".$issue."' maxlength=10/>
 <tr><td><font face=arial size=3><b>Orientation \\ Ориентация скана</b> <font size=2 color=gray>(landscape, portrait)</font></font><td><input type='text' name='Orientation' size=15 value='".$orientation."' maxlength=50/>
@@ -230,10 +201,9 @@ search in:
 <tr><td><font face=arial size=3><b>Cleaned \\ Обрезанный скан</b> <font size=2 color=gray>(yes, no)</font></font><td><input type='text' name='Cleaned' size=10 value='".$cleaned."' maxlength=50/>
 <tr><td><font face=arial size=3><b>Commentary \\ Комментарий</b> <font size=2 color=gray>(5000 символов макс.)</font></font><td><input type='text' name='Commentary' size=90 value='".$commentary."' maxlength=5000/>
 <tr><td><font face=arial size=3><b>MD5 of a Better Version \\ MD5 лучшей версии</b></font><td><input type='text' name='Generic' size=35 value='".$generic."' maxlength=32/>
+<tr><td><font face=arial size=3><b>CoverURL \\ Ссылка на обложку</b> <font size=2 color=gray></font></font><td><input type='text' name='Coverurl' size=90 value='".$coverurl."' maxlength=199/>
 <tr><td><font face=arial size=3><b><a href='http://en.wikipedia.org/wiki/Universal_Decimal_Classification'>UDC \\ УДК</a></b></font><td><input type='text' name='UDC' size=90 value='".$udc."' maxlength=50/>
 <tr><td><font face=arial size=3><b><a href='http://www.indiana.edu/~libslav/slavcatman/bbkover.html'>LBC \\ ББК</a></b></font><td><input type='text' name='LBC' size=90 value='".$lbc."' maxlength=50/>
-<tr><td><font face=arial size=3><b>Cover \\ Обложка</b> <font size=2 color=gray>(URL)</font></font><td><input type='text' name='Coverurl' size=90 value='".$coverurl."' maxlength=500/>
-
 <tr><td><font face=arial size=3 color=gray><b>Filesize \\ Размер файла</b> <font size=2>(bytes)</font></font><td><input readonly type='text' name='Filesize' size=10 value='".$filesize."' maxlength=20/>
 <tr><td><font face=arial size=3 color=gray><b>MD5</b></font><td><input readonly type='text' name='MD5' size=35 value='".$md5."' maxlength=32/>
 <tr><td><font face=arial size=3 color=gray><b>File Extension \\ Расширение файла</b></font><td><input readonly type='text' name='Extension' size=10 value='".$fileext."' maxlength=50/>
@@ -263,7 +233,7 @@ search in:
 
 	// edit, if MD5 found
 	if ($_POST['Form'] == 2){
-		if ($editing || isset($_POST['amazon']) || isset($_POST['ozon']) || isset($_POST['rgb'])) echo $regform;
+		if ($editing || isset($_POST['amazon']) || isset($_POST['ozon'])) echo $regform;
 		else echo $htmlhead."<font color='#A00000'><h1>Book not found</h1></font>There is no such book in the database.<br>You are welcome to upload this piece!<p><a href='registration.php'>Go back to the upload page</a><p><h2>Thank you!</h2>".$htmlfoot;
 	}
 ?>
--- register.php
+++ register.php
@@ -25,7 +25,7 @@
 	if ($pages == '') $pages = 0;
 	if ($issue == '') $issue = 0;
 	if ($dpi == '') $dpi = 0;
-	if ($identifier == 'ISBN10:') $identifier = '';
+	if ($identifier == 'ISBN ') $identifier = '';
 
 	// escape single quotes
 
@@ -54,9 +54,9 @@
 	$commentary = clean('Commentary');
 	$series = clean('Series');
         $periodical = clean('Periodical');
+        $coverurl = clean('Coverurl');
 	$udc = clean('UDC');
 	$lbc = clean('LBC');
-        $coverurl = clean('Coverurl');
 
 	// open file read-only and lock before SQL-query
 	if (!$_POST['Edit']){
@@ -67,10 +67,10 @@
 
 		if (!flock($h,LOCK_EX)) die("<p>Cannot lock temporary file '".$file."'");
 
-		$sql="INSERT INTO $dbtable (ID,Topic,Author,Title,VolumeInfo,Year,Publisher,City,Edition,Identifier,Pages,Filesize,Issue,Orientation,DPI,Color,Cleaned,Language,MD5,Extension,Library,Commentary,Series,Periodical,UDC,LBC,Coverurl) VALUES
-		('$id','$topic','$author','$title','$volinfo','$year','$publisher','$city','$edition','$identifier','$pages','$_POST[Filesize]','$issue','$orientation','$dpi','$color','$cleaned','$language','$_POST[MD5]','$_POST[Extension]','$library','$commentary','$series','$periodical','$udc','$lbc','$coverurl')";
+		$sql="INSERT INTO $dbtable (ID,Topic,Author,Title,VolumeInfo,Year,Publisher,City,Edition,Identifier,Pages,Filesize,Issue,Orientation,DPI,Color,Cleaned,Language,MD5,Extension,Library,Commentary,Series,Periodical,Coverurl,UDC,LBC) VALUES
+		('$id','$topic','$author','$title','$volinfo','$year','$publisher','$city','$edition','$identifier','$pages','$_POST[Filesize]','$issue','$orientation','$dpi','$color','$cleaned','$language','$_POST[MD5]','$_POST[Extension]','$library','$commentary','$series','$periodical','$coverurl','$udc','$lbc')";
 	} else {
-		$sql="UPDATE $dbtable SET `Generic`='$generic',`Topic`='$topic',`Author`='$author',`Title`='$title',`VolumeInfo`='$volinfo',`Year`='$year',`Publisher`='$publisher',`City`='$city',`Edition`='$edition',`Identifier`='$identifier',`Pages`='$pages',`Issue`='$issue',`Orientation`='$orientation',`DPI`='$dpi',`Color`='$color',`Cleaned`='$cleaned',`Language`='$language',`Extension`='$_POST[Extension]',`Library`='$library',`Commentary`='$commentary',`Series`='$series',`Periodical`='$periodical',`UDC`='$udc',`LBC`='$lbc',`Coverurl`='$coverurl' WHERE `MD5`='$_POST[MD5]' LIMIT 1";
+		$sql="UPDATE $dbtable SET `Generic`='$generic',`Topic`='$topic',`Author`='$author',`Title`='$title',`VolumeInfo`='$volinfo',`Year`='$year',`Publisher`='$publisher',`City`='$city',`Edition`='$edition',`Identifier`='$identifier',`Pages`='$pages',`Issue`='$issue',`Orientation`='$orientation',`DPI`='$dpi',`Color`='$color',`Cleaned`='$cleaned',`Language`='$language',`Extension`='$_POST[Extension]',`Library`='$library',`Commentary`='$commentary',`Series`='$series',`Periodical`='$periodical',`Coverurl`='$coverurl',`UDC`='$udc',`LBC`='$lbc' WHERE `MD5`='$_POST[MD5]' LIMIT 1";
 	}
 
 	if (!mysql_query($sql,$con))
@@ -105,7 +105,7 @@
 function clean($var){
 	$c = "'\\";
 	$str = str_replace("\t",' ',$_POST[$var]); // replace tabs
-//	$str = preg_replace('/\s\s+/',' ',$str); // delete multiple spaces
+//   $str = preg_replace('/\s\s+/',' ',$str); // delete multiple spaces
 	return trim(addcslashes($str,$c));
 }
 
